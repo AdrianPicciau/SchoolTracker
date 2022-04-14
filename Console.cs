65c1fd108811ec24fb0bbf5cfed999f8b8b742ea
Adrian Picciau <alp1g18@soton.ac.uk>
2022-04-14 11:04:24 +0100

    Added C# file

---
+++ Console.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Util
+{
+    class Console
+    {
+        // method for asking question and returning answer
+        static public string Ask(String question)
+        {
+            System.Console.Write(question);
+            return System.Console.ReadLine();
+        }
+    }
+}
